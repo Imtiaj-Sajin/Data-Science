c80f31af0439fa98a79efa11d226019f0eac5f90
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-13 15:23:07 +0600

    Create dataframe.r

---
+++ dataframe.r
@@ -0,0 +1,21 @@
+pId<-c(3,0,6)
+age=c(8,6,4)
+diabetes=c("Type1","Type1","")
+
+patientData=data.frame(pId,age,diabetes)
+patientData
+
+bg=c("A+","b+","o-")
+
+patientData$BloodGroup=bg
+
+patientData
+
+
+
+install.packages("tibble")
+library(tibble)
+
+g=c('M','f','f')
+patientData=add_column(patientData, g, .after = 2)
+patientData
