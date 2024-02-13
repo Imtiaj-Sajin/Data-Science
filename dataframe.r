f39311486bbbb2a928da6ce07cc03bb06005c075
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-13 15:31:40 +0600

    Update dataframe.r

--- dataframe.r
+++ dataframe.r
@@ -1,6 +1,6 @@
 pId<-c(3,0,6)
 age=c(8,6,4)
-diabetes=c("Type1","Type1","")
+diabetes=c("Type1","Type2","Type1")
 
 patientData=data.frame(pId,age,diabetes)
 patientData
@@ -19,3 +19,7 @@ library(tibble)
 g=c('M','f','f')
 patientData=add_column(patientData, g, .after = 2)
 patientData
+
+newrow=c(2, 6, 'f',    'Type3',        'b+')
+updatedTable=rbind(patientData,newrow)
+updatedTable
