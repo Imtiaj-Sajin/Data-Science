551a1dbe4ee8e4a2f189119ae54f5d9928e5006f
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-13 16:34:51 +0600

    Update list.r
    list append added

--- list.r
+++ list.r
@@ -1,9 +1,15 @@
-
+#create list
 g<-"First List"
 h<-c(23,34,4,9)
 j<-matrix(1:10,nrow = 5)
 k<-c("one","two","four")
 
 myList<-list(title=g,ages=h,j,k)
-
 myList
+
+
+#add item
+list<-list("orange","banana","mango")
+list
+list=append(list,"Olive")
+list
