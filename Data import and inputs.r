bca983359435733cdcfd1f425e97779da1c6c5a5
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-20 16:50:12 +0600

    Update Data import and inputs.r

--- Data import and inputs.r
+++ Data import and inputs.r
@@ -54,3 +54,30 @@ mydata$g=factor(mydata$g,
                 labels = c(1,2)) #values convert to 1,2
 mydata
 
+
+#summery of the stucture of dataset
+str(csvData)
+str(mydata)
+
+#descriptive statistics using Summery() Function
+summary(mydata)
+summary(csvData)
+
+salary=read.csv("C:/Users/student/Downloads/position_salaries_dataset.csv",header=TRUE, sep=",") #your csv file location
+summary(salary)
+
+
+#standard deviation (column wise)
+sd(salary$Salary)
+
+#standard deviation with if numeric(column wise)
+install.packages("dplyr") #deep layer
+library(dplyr)
+
+salary %>% summarise_if(is.numeric,sd)
+
+#standard deviation (row wise)
+install.packages("matrixStats")
+library(matrixStats)
+salary$score=rowSds(as.matrix(salary[,c(2,3)]))
+salary
