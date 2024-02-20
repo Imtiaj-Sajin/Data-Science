d652c2463cde2ad92c96c55ea7203c99f497c458
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-20 14:40:11 +0600

    Create User input Types.r

---
+++ User input Types.r
@@ -0,0 +1,15 @@
+#user input 
+
+##readline
+var1=readline(prompt="Enter any value:")
+var2=readline(prompt = "enter any number:")
+
+var2=as.integer(var2)
+
+print(var1)
+print(var2)
+
+
+##scan()
+x=scan()
+print(x)
