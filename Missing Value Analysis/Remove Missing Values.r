4200e8a5706795ffedd1d60117b8500246fd5b32
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-03-05 14:51:34 +0600

    Create Remove Missing Values.r

---
+++ Missing Value Analysis/Remove Missing Values.r
@@ -0,0 +1,10 @@
+#import from csv
+csvData=read.csv("C:/Users/student/Downloads/loan_data_test.csv",header=TRUE, sep=",") 
+csvData
+csvData=edit(csvData)
+
+myData=csvData
+
+
+#delete missing value by omit
+remove=na.omit(myData) #(delete all rows where data is NULL)
